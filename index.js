49b425d75bf8eb93761a6ce4b5931b7124cfe9ea
Wazed Biplob <www.biplob@gmail.com>
2023-06-08 19:55:42 +0600

    Classes API imeplemented

--- index.js
+++ index.js
@@ -10,6 +10,61 @@ app.use(express.json());
 
 const data = require("./data.json");
 
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.dp2hutp.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+const userCollection = client.db("golden-tunes").collection("users");
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: "Token Not Found" });
+  }
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: "Token Mismatch" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
+// jwt token
+app.post("/jwt", (req, res) => {
+  const user = req.body;
+  console.log(user);
+  const token = jwt.sign(user, process.env.ACCESS_TOKEN, {
+    expiresIn: 3600,
+  });
+  res.send({ token });
+});
+// verifying jwt
+
 // checking if the server is running
 app.get("/", (req, res) => {
   res.send("finely tuned");
@@ -17,6 +72,30 @@ app.get("/", (req, res) => {
 // sending demon class data
 app.get("/classes", (req, res) => {
   res.send(data);
+});
+app.get("/users/admin/:email", async (req, res) => {
+  const query = { email: req?.params?.email };
+  console.log(`query`, query);
+  const user = await userCollection.findOne(query);
+  const result = { role: user.role };
+  console.log(result);
+  res.send(result);
+});
+app.get("/users", verifyJWT, async (req, res) => {
+  const result = await userCollection.find().toArray();
+  res.send(result);
+});
+app.post("/users", async (req, res) => {
+  const user = req.body;
+  const query = { email: user.email };
+  const existingUser = await userCollection.findOne(query);
+  if (existingUser) {
+    console.log(`User Exists`);
+    return;
+  } else {
+    const result = await userCollection.insertOne(user);
+    res.send(result);
+  }
 });
 app.listen(port, () => {
   console.log(`tuned at ${port}`);
